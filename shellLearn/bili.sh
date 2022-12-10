445a34382376fa2c863266d5261fc43da37c6623
Zhou, Hang <hang.zhou@intel.com>
2022-12-10 02:22:42 -0500

    test

--- shellLearn/bili.sh
+++ shellLearn/bili.sh
@@ -47,15 +47,15 @@
 # done
 
 
-DATE=$(date +%d/%b/%Y:%H:%M)
-LOG_FILE=/usr/local/nginx/logs/demo2.access.log
-ABNORMAL_IP=$(tail -n5000 $LOG_FILE |grep $DATE |awk '{a[$1]++}END{for(i in a)if(a[i]>10)print i}')
-for IP in $ABNORMAL_IP; do
-    if [ $(iptables -vnL |grep -c "$IP") -eq 0 ]; then
-        iptables -I INPUT -s $IP -j DROP
-        echo "$(date +'%F_%T') $IP" >> /tmp/drop_ip.log
-    fi
-done
+# DATE=$(date +%d/%b/%Y:%H:%M)
+# LOG_FILE=/usr/local/nginx/logs/demo2.access.log
+# ABNORMAL_IP=$(tail -n5000 $LOG_FILE |grep $DATE |awk '{a[$1]++}END{for(i in a)if(a[i]>10)print i}')
+# for IP in $ABNORMAL_IP; do
+#     if [ $(iptables -vnL |grep -c "$IP") -eq 0 ]; then
+#         iptables -I INPUT -s $IP -j DROP
+#         echo "$(date +'%F_%T') $IP" >> /tmp/drop_ip.log
+#     fi
+# done
 # echo
 # echo -e "\tMenu\n"
 # echo -e "1. Install Nginx" &&echo -e "2. Installdfsa PdfsHP"
@@ -66,6 +66,30 @@ done
 
 
 
-#!/usr/bin/env bash
+# #!/bin/bash
 
-pwd
+# for arg in elem1 elem2 ... elemN
+# do
+#     ./cat.sh --search $arg | tee -a search.log
+# done
+
+# !/bin/sh
+# BIOS_VERSION=`sudo dmidecode -t bios|grep Version`
+# # xmlcli for server only platform EGS onwards will support.
+# BACKEND=xmlcli
+# lscpu | grep -q amx
+# if [ $? != 0 ]; then
+#     BACKEND=syscfg
+#     if [[ "$BIOS_VERSION" =~ "CRB" ]]; then
+#         BACKEND=xmlcli
+#     fi
+#     if [[ $BACKEND = "syscfdfg" ]]; then
+#         # ./syscfg /s ${SYSTEM_NAME}.ini /b  
+#         echo 100afd
+#     fi
+# fi
+
+if ! command -v conda >/dev/null 2>&1; then                  
+        echo "start install miniconda......"               
+        echo 100
+fi   
--- shellLearn/cat.sh
+++ shellLearn/cat.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
-pwd 
-hostname
+# pwd 
+# hostname
 
 
-Required_HugePage=$1
-Required_Size=$2
+# Required_HugePage=$1
+# Required_Size=$2
 
-HugePages_Total=$(grep HugePages_Total /proc/meminfo | awk '{print $2}')
-Hugepagesize=$(grep Hugepagesize /proc/meminfo | awk '{print $2}')
-HugePages_Free=$(grep HugePages_Free /proc/meminfo | awk '{print $2}')
-
-
-touch a.txt
-echo $HugePages_Total $Hugepagesize $HugePages_Free 
-
-echo $Required_HugePage $Required_Size
-
-touch b.txt
+# HugePages_Total=$(grep HugePages_Total /proc/meminfo | awk '{print $2}')
+# Hugepagesize=$(grep Hugepagesize /proc/meminfo | awk '{print $2}')
+# HugePages_Free=$(grep HugePages_Free /proc/meminfo | awk '{print $2}')
 
 
+# touch a.txt
+# echo $HugePages_Total $Hugepagesize $HugePages_Free 
+
+# echo $Required_HugePage $Required_Size
+
+# touch b.txt
 
 
+
+
+# case $1 in
+#     --search) 
+#         echo 
+#     --find) 
+#         $PYTHON_CMD -find $* 2>&1 | tee $LOG_NAME;
+#         exit $? ;;
+#     --set) 
+#         warning ;
+#         CONF_PATH=$2; shift;
+#         ( $PYTHON_CMD -set $CONF_PATH $* 2>&1; echo $? > status ) | tee $LOG_NAME; 
+#         if [ `cat status` $EQ 0 ];  then 
+#             if [ $BACKEND $EQ 'xmlcli' ]; then
+#                 reboot
+#             else 
+#                 /bin/bash syscfg_run.sh
+#                 if [ $? $EQ 0 ];  then 
+#                     rm -f syscfg_run.sh
+#                     reboot
+#                 fi
+#             fi
+#         fi ;
+#         exit $? ;;
+# esac
