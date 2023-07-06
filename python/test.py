4f63e4e82b6d720aed9f95b4b345daf97114cd09
hangzho1 <hang.zhou@intel.com>
2023-07-05 22:46:56 -0400

    update

--- python/test.py
+++ python/test.py
@@ -735,10 +735,6 @@ print(max(dp))
 
 
 
-<<<<<<< HEAD
-# from datetime import datetime
-# import datetime
-=======
 from distutils.command.config import config
 import yaml
 
@@ -786,7 +782,6 @@ print("value of arg {}".format(arg),len(arg))
 x = ["{}={}".format(setting['knob'], setting['value']) for setting in settings]
 print("value of x {}".format(x),len(x))
 
->>>>>>> 8417ccfab69ac06672699f6ea37edc4575b79594
 
 
 knobs_to_change = [setting['knob'] for setting in settings]
